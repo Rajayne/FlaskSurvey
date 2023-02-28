c2b150f215ed1bc7a544de36e7320948b5f2f51c
Rajayne <ryann.j.nealey@gmail.com>
2023-02-27 22:25:09 -0800

    Created session response to track answers and prevent user from jumping to different questions, and auto-fill html for each question using variables

--- app.py
+++ app.py
@@ -1,52 +1,64 @@
 from flask import Flask, request, render_template, redirect, flash, jsonify, session
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
 
 # Create templates folder and tell Flask to use it
 app = Flask(__name__,template_folder='templates')
-
 app.debug = True
-# Debug Toolbar Requirement
 app.config['SECRET_KEY'] = 'key'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-from surveys import satisfaction_survey
+RESPONSES_KEY = "responses"
+
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def show_start():
+    return render_template('start.html', survey=survey)
 
-RESPONSES = []
 
-# QUESTIONS = {
-#     0: "Question 1",
-#     1: "Question 2",
-#     2: "Question 3",
-#     3: "Question 4",
-#     4: "Question 5",
-# }
+@app.route('/begin', methods=["POST"])
+def start_survey():
+    """Clear session of responses."""
+    session[RESPONSES_KEY] = []
 
-# Int: indicates id should be converted to integer
-# .get(var, alt) if var not found will return alt i.e. no post 5
-# @app.route('/question/<int:id>')
-# def find_question(id):
-#     question = QUESTIONS.get(id, "Question not found")
-#     return f"<p>{question}</p>"
+    return redirect("/question/0")
 
-@app.route('/question')
-def find_question():
-    return render_template('question.html')
+
+@app.route('/question/<int:id>')
+def show_question(id):
+    """Display current question using id"""
+    responses = session.get(RESPONSES_KEY)
+
+    if (responses is None):
+        # Accessing question page too soon
+        return redirect("/")
+    if (len(responses) == len(survey.questions)):
+        # Already answered all questions
+        return redirect("/end")
+    if (len(responses) != id):
+        # Tryin to access questions out of order
+        flash(f"Please answer survey question below to continue.")
+        return redirect(f"/question/{len(responses)}")
+
+    question = survey.questions[id]
+    return render_template(
+        "question.html", question_num=id, question=question)
+
 
 @app.route('/answer', methods=["POST"])
 def save_answer():
-    answer = request.form.get('answer',None)
-    if answer == None:
-        flash('Please select an option!')
-        return redirect('/question')
-    elif answer in RESPONSES:
-        flash("Question already answered!", "error")
-        return redirect('/')
+    choice = request.form['answer']
+    
+    responses = session[RESPONSES_KEY]
+    responses.append(choice)
+    session[RESPONSES_KEY] = responses
+       
+    if (len(responses) == len(survey.questions)):
+        return redirect('/end')
     else:
-        RESPONSES.append(answer)
-        flash("Question answered!", "success")
-        return redirect('/question')
+        return redirect(f"/question/{len(responses)}")
+
+@app.route('/end')
+def thankyou():
+    return render_template('end.html')
