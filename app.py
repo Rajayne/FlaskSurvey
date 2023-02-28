cc52de194a19c939ced312e01012ef8cb9c1bca5
Rajayne <ryann.j.nealey@gmail.com>
2023-02-27 18:44:33 -0800

    Create base template, css static template, and home page

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template, redirect, flash, jsonify, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+# Create templates folder and tell Flask to use it
+app = Flask(__name__,template_folder='templates')
+
+app.debug = True
+# Debug Toolbar Requirement
+app.config['SECRET_KEY'] = 'key'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+from surveys import satisfaction_survey
+
+@app.route('/')
+def home():
+    return render_template('home.html')
